a32aab6225632369e0a4f7570470dd60af1c8e66
ROVIN <91716446+URK21CS3004@users.noreply.github.com>
2022-09-12 16:31:43 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,16 +1,14 @@
 from typing import List
 
-def selectionSort(array, size) -> List[int]:  
-  
-  for i in range(1,size-1):
-    small = i
-    for index in range(i + 1,size):
-      if array[index] < array[small]:
-        min = j
-        array[i],array[min]=array[min],array[i]    
-  return array     
+def selectionSort(array, size) -> List[int]:
+  for select in range(size):
+    check = select
+    for sort in range(select,size):
+      if(array[sort]<array[select]):
+        array[sort],array[select] = array[select],array[sort]
+  return array
 
-# Do not change the following code
+# Do not change the following code//
 input_data = input()
 data = []
 for item in input_data.split(', '):
